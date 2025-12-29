6d8a16eccbf9a6855de5624f313d60a5e7058656
Adixx73 <adithyabalaji757@gmail.com>
2025-12-29 14:38:02 +0530

    Refactor CarModel year field and formatting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -12,8 +10,9 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -24,11 +23,13 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+            MinValueValidator(2015),
+        ],
+    )
 
     def __str__(self):
         return self.name  # Return the name as the string representation
